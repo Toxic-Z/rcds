447047763a3bc4e1716895df47310f2a2252a303
Zander Work <zander@zanderwork.com>
2023-03-30 22:31:31 -0400

    fuck python bytestring

--- rcds/backends/rctf/rctf.py
+++ rcds/backends/rctf/rctf.py
@@ -13,7 +13,7 @@ class RCTFAdminV1:
     def __init__(self, endpoint: str, login_token: Optional[str]):
         print(f"--- setting up rctfadminv1 object")
         print(f"--- endpoint: {endpoint}")
-        print(f"--- login_token: {b64encode(login_token).decode() if login_token is not None else '<none>'}")
+        print(f"--- login_token: {b64encode(login_token.encode()).decode() if login_token is not None else '<none>'}")
 
         self.session = BaseUrlSession(urljoin(endpoint, "api/v1/admin/"))
 
